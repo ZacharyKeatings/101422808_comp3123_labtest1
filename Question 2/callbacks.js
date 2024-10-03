1e8fa89f15a521e9d152736f0d765f94a753fffe
Zachary Keatings <74317231+ZacharyKeatings@users.noreply.github.com>
2024-10-03 17:53:53 -0400

    Completed question 2

---
+++ Question 2/callbacks.js
@@ -0,0 +1,31 @@
+const resolvedPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ message: 'delayed success!' });
+        }, 500);
+    });
+};
+
+const rejectedPromise = () => {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject('error: delayed exception!');
+        }, 500);
+    });
+};
+
+(async () => {
+    try {
+        const result = await resolvedPromise();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+
+    try {
+        const result = await rejectedPromise();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+})();
